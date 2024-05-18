679994fefb7ec737a972add2522db6f8413f6d0c
Eduardo Corgniali <educorgniali@gmail.com>
2024-05-18 15:43:58 -0300

    feat: se agrega logo y link de instagram

--- index.php
+++ index.php
@@ -3,8 +3,11 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <!-- favicon -->
+  <link rel="shortcut icon" href="img/favicon.ico" type="image/x-icon">
   <title>Fix Us Consultant | Página en Construcción</title>
   <script src="https://cdn.tailwindcss.com"></script>
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
   <script>
     tailwind.config = {
       theme: {
@@ -31,23 +34,18 @@
 </head>
 <body class="min-h-screen bg-gray-100 flex flex-col items-center justify-center relative px-4">
     <header class="flex justify-center">
-      <img src="https://imagen.io/" alt="Logo de la Consultora">
+      <img style="max-width: 350px" src="./img/logo_fixus.png" alt="Logo de la Consultora">
     </header>
     <main class="text-gray-800 py-12">
       <h1 class="text-3xl text-center font-bold mb-4">¡Próximamente!</h1>
       <h2 class="text-2xl text-center font-medium mb-4">Estamos trabajando en algo emocionante...</h2>
-      <p class="text-center mb-8">Te invitamos a que te suscribas a nuestro boletín informativo para que seas el primero en enterarte de nuestro lanzamiento.</p>
-      <form action="#">
-        <div class="flex flex-col items-center">
-          <input type="email" placeholder="Tu correo electrónico" class="border-2 border-gray-300 p-2 rounded w-full max-w-md">
-          <button type="submit" class="bg-cotton-candy-300 p-2 rounded mt-4 w-full max-w-md">Suscribirme</button>
-        </div>
-      </form>
-      <p class="text-center mt-8">También puedes seguirnos en nuestras redes sociales:</p>
+      <p class="text-center mb-8">Mientras tanto, te invitamos a que nos sigas en nuestras redes para no perderte de nada:</p>
       <ul class="flex justify-center gap-2 mt-4">
-        <li><a href="#">Facebook</a></li>
-        <li><a href="#">Twitter</a></li>
-        <li><a href="#">Instagram</a></li>
+        <li>
+          <a href="https://instagram.com/fixusconsultants" class="text-gray-700 hover:text-gray-900">
+            <i class="fab fa-instagram text-4xl"></i>
+          </a>
+        </li>
       </ul>
     </main>
     <footer class="text-gray-600 p-4 flex flex-col items-center justify-center">
