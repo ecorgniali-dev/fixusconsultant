79012c5dcc50268e514733ba9a50a3f8d6d1d3e8
Eduardo Corgniali <educorgniali@gmail.com>
2024-10-24 12:26:29 -0300

    suma página brief para desarrollo web

---
+++ brief-desarrollo-web.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+<meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="robots" content="noindex, nofollow">
+    <link rel="shortcut icon" href="img/favicon.ico" type="image/x-icon">
+    <title>Fix Us Consultant | Briefing Desarrollo Web</title>
+    <style>
+        /* body {
+            font-family: sans-serif;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+        } */
+        .google-form-contenedor {    
+            width: 100%; 
+            overflow: hidden; 
+            padding-top: 100%;
+        } 
+        
+        .responsive-iframe { 
+            position: absolute; 
+            top: 0; left: 0; 
+            bottom: 0; 
+            right: 0; 
+            width: 100%; 
+            height: 100%; 
+            border: none; 
+        } 
+    </style>
+</head>
+<body>
+    <div class="google-form-contenedor">
+        <iframe class="responsive-iframe" src="https://docs.google.com/forms/d/e/1FAIpQLSdATNYqQkr5MOLWc5_77Ehgutro7eObyp5tpsYEI6O6dyoakQ/viewform?embedded=true" frameborder="0" marginheight="0" marginwidth="0">Cargando…</iframe>
+    </div>
+</body>
+</html>
